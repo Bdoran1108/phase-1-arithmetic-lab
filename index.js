f5f7e27d5fa901f71d258c04ee65c80cb9923f92
Brian <Briandoran1@icloud.com>
2021-10-11 17:31:32 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+let num1=31
+let num2=2
+let multiply = (num1 * num2)
+
+let num3=40
+let num4=10
+let mod = (num3 / num4)
+
+let max = 20
+
+let random = Math.floor(Math.random()*31)+1;
